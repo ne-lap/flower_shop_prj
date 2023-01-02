48d9f82733b81df2fc5f5c34c1b8889625175e69
ne-lap <nelapetrovic@hotmail.com>
2023-01-02 22:23:36 +0100

    dopuna forme + osnovni CSS

--- script.js
+++ script.js
@@ -6,39 +6,95 @@ let ruza = document.getElementById('ruze');
 let ljiljan = document.getElementById('ljiljani');
 let gerber = document.getElementById('gerberi');
 
+let bombonjeraCheck = document.getElementById('bombonjera');
+let cokoladaCheck = document.getElementById('cokolada');
+let sampanjacCheck = document.getElementById('sampanjac');
 
 btnSubmit.addEventListener('click', () => {
     let naslov = document.createElement('h2');
     naslov.textContent = 'Vaša porudžbina:';
     porudzbina.appendChild(naslov);
     
+    let sum = 0
+
     let ruzaNo = ruza.value;
     let ljiljanNo = ljiljan.value;
     let gerberNo = gerber.value;
-    console.log(ruzaNo);
-    console.log(ljiljanNo);
-    console.log(gerberNo);
-    
+
+    let picContainerRose = document.createElement('div');
+    porudzbina.append(picContainerRose);
     for(let i=1; i<=ruzaNo; i++) {
+        
         let img = document.createElement('img');
         img.src = 'photos/rose.png';
         img.classList.add('flower');
-        porudzbina.append(img);
+        picContainerRose.append(img);
+        sum += 150;
     }
-    
+    let picContainerLily = document.createElement('div');
+    porudzbina.append(picContainerLily);
     for(let i=1; i<=ljiljanNo; i++) {
         let img = document.createElement('img');
         img.src = 'photos/lily.png';
         img.classList.add('flower');
-        porudzbina.append(img);
+        picContainerLily.append(img);
+        sum += 120;
     }
-
+    let picContainerGarbera = document.createElement('div');
+    porudzbina.append(picContainerGarbera);
     for(let i=1; i<=gerberNo; i++) {
         let img = document.createElement('img');
         img.src = 'photos/gerbera.png';
         img.classList.add('flower');
-        porudzbina.append(img);
+        picContainerGarbera.append(img);
+        sum += 70;
     }
 
+    if(bombonjeraCheck.checked) {
+        let poklon = document.createElement('p');
+        poklon.textContent = '+ bombonjera';
+        porudzbina.append(poklon);
+        sum += 500;
+    }
 
+    if(cokoladaCheck.checked) {
+        let poklon = document.createElement('p');
+        poklon.textContent = '+ čokolada';
+        porudzbina.append(poklon);
+        sum += 500;
+    }
+
+    if(sampanjacCheck.checked) {
+        let poklon = document.createElement('p');
+        poklon.textContent = '+ šampanjac';
+        porudzbina.append(poklon);
+        sum += 500;
+    }
+
+    let placanje = document.querySelector(`input[type="radio"]:checked`).value;
+
+    if(placanje == 1) {
+        let cena = document.createElement('p');
+        cena.textContent = `Cena je ${sum}`;
+        porudzbina.append(cena)
+    }
+    else {
+        if(sum > 2000) {
+            let cena = document.createElement('p');
+            let cenaPopust = document.createElement('p');
+            cena.textContent = `Cena je ${sum}`;
+            cenaPopust.textContent = `Cena sa popustom je ${sum*0.9}`;
+            porudzbina.append(cena, cenaPopust);
+        }
+        else {
+            let cena = document.createElement('p');
+            cena.textContent = `Cena je ${sum}`;
+            porudzbina.append(cena);
+        }
+    }
+    btnSubmit.disabled = true;
+});
+
+btnReset.addEventListener('click', () => {
+    location.reload();
 });
